80116cc370b884928e7b1304dd7c5e714504066f
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-03-29 22:57:10 +0300

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,24 @@
 import { PrivateRoute } from 'hoc/PrivateRoute';
 import { PublicRoute } from 'hoc/PublicRoute';
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Route, Routes } from 'react-router';
 import { refreshUser } from 'redux/auth/operations';
-import { selectIsRefreshing } from 'redux/auth/selector';
+// import { selectIsRefreshing } from 'redux/auth/selector';
 
 import Home from './Home';
 import Layout from './Layout';
-import Loader from './Loader';
+// import Loader from './Loader';
 import Login from './Login';
 import Phonebook from './Phonebook';
 import Register from './Register';
 
-import style from './app.module.scss';
+// import style from './app.module.scss';
 
 export const App = () => {
   const dispatch = useDispatch();
 
-  const isLoading = useSelector(selectIsRefreshing);
+  // const isLoading = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
