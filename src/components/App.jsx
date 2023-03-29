f8c0396d45327044f411663061a86a12168ae339
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-03-29 14:52:34 +0300

    commit Loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,34 @@
 import { PrivateRoute } from 'hoc/PrivateRoute';
 import { PublicRoute } from 'hoc/PublicRoute';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router';
 import { refreshUser } from 'redux/auth/operations';
+import { selectIsRefreshing } from 'redux/auth/selector';
+
 import Home from './Home';
 import Layout from './Layout';
+import Loader from './Loader';
 import Login from './Login';
 import Phonebook from './Phonebook';
 import Register from './Register';
 
+import style from './app.module.scss';
+
 export const App = () => {
   const dispatch = useDispatch();
+
+  const isLoading = useSelector(selectIsRefreshing);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
+  return isLoading ? (
+    <div className={style.loaderApp}>
+      <Loader />
+    </div>
+  ) : (
     <div>
       <Routes>
         <Route element={<Layout />}>
